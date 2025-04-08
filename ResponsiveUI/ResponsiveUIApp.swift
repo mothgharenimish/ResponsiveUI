7f444aadcb8d3ff28957b3d3a2e48f28c0e052c4
mothgharenimish <mothgharenimish0@gmail.com>
2025-04-08 12:50:46 +0530

    Initial Commit

---
+++ ResponsiveUI/ResponsiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResponsiveUIApp.swift
+//  ResponsiveUI
+//
+//  Created by Nimish Mothghare on 08/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ResponsiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ResponsiveUITests/ResponsiveUITests.swift
@@ -0,0 +1,17 @@
+//
+//  ResponsiveUITests.swift
+//  ResponsiveUITests
+//
+//  Created by Nimish Mothghare on 08/04/25.
+//
+
+import Testing
+@testable import ResponsiveUI
+
+struct ResponsiveUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
